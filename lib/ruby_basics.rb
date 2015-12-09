3fb90cd20c44cfbda890d00a062686efd464536e
rramdehol <rramdeholshift@gmail.com>
2015-12-08 22:48:28 -0500

    Added ruby_basics.rb

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+  answer = (num1/num2)
 end
 
 def assign_variable(value)
-  
+  name = value 
+  name 
 end
 
-def argue
-  
+def argue (phrase)
+  phrase 
 end
 
-def greeting
-
+def greeting (greeting, name)
+  greeting = greeting + name 
+  greeting
 end
 
 def return_a_value
-
+  phrase = "Nice"
+  phrase
 end
 
 def last_evaluated_value
-
+  last_evaluated_value = "expert"
+  last_evaluated_value
 end
 
-def pizza_party
-
+def pizza_party (topping = "cheese")
+  topping
 end
